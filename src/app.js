2e6be5f9bd04b765bc477911b0972768645e6680
Juanita Romero <Romerojuanita9@gmail.com>
2023-01-15 22:08:31 -0600

    Added relevant icon from API

--- src/app.js
+++ src/app.js
@@ -1,3 +1,27 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   let temperatureElements = document.querySelector("#current_temp");
   let cityElement = document.querySelector("#current_city");
@@ -5,6 +29,8 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let feels_likeElement = document.querySelector("#feels_like");
   let windElement = document.querySelector("#wind_speed");
+  let dateElement = document.querySelector("#date_time");
+  let iconElement = document.querySelector("#icon");
 
   temperatureElements.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
@@ -14,6 +40,9 @@ function displayTemperature(response) {
     response.data.temperature.feels_like
   );
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
+  iconElement.setAttribute("src", response.data.condition.icon_url);
+  iconElement.setAttribute("alt", response.data.condition.description);
 }
 
 let apiKey = "4a71f359eca9d0eft630db05oc4deac4";
